0e4d92af492ceb418ff88a60f9d94a801b06ea18
Fábio Santos <frs@isep.ipp.pt>
2024-02-24 18:07:50 +0000

    First version of DemoORM

---
+++ DemoORM/src/main/java/isep/eapli/demo_orm/Main.java
@@ -0,0 +1,11 @@
+package isep.eapli.demo_orm;
+
+import isep.eapli.demo_orm.apresentacao.MainMenu;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        MainMenu.mainLoop();
+    }
+}
